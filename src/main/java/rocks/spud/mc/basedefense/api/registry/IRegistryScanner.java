087bfa2d3e431d83ee4e9a3f91af6a9efea7307a
Johannes Donath <johannesd@torchmind.com>
2015-01-18 03:39:01 +0100

    Reverted original attempt to fix errors caused by faulty annotations ... gnarf

--- src/main/java/rocks/spud/mc/basedefense/api/registry/IRegistryScanner.java
+++ src/main/java/rocks/spud/mc/basedefense/api/registry/IRegistryScanner.java
@@ -19,11 +19,13 @@ package rocks.spud.mc.basedefense.api.registry;
 import cpw.mods.fml.relauncher.Side;
 import rocks.spud.mc.basedefense.api.error.RegistryRegistrationException;
 
+import java.lang.annotation.Annotation;
+
 /**
  * Provides a base definition for registry scanners.
  * @author {@literal Johannes Donath <johannesd@torchmind.com>}
  */
-public interface IRegistryScanner<T> {
+public interface IRegistryScanner<T extends Annotation> {
 
 	/**
 	 * Returns the scanner side.
