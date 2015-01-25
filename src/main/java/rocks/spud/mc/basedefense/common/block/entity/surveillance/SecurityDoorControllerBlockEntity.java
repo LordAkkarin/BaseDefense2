3babc1e52c3507605c2b89ab94685407fb34fefd
Johannes Donath <johannesd@torchmind.com>
2015-01-25 17:30:14 +0100

    Added door opening/closing and behavior settings

--- src/main/java/rocks/spud/mc/basedefense/common/block/entity/surveillance/SecurityDoorControllerBlockEntity.java
+++ src/main/java/rocks/spud/mc/basedefense/common/block/entity/surveillance/SecurityDoorControllerBlockEntity.java
@@ -25,18 +25,26 @@ import appeng.api.networking.events.MENetworkPowerStatusChange;
 import appeng.api.networking.pathing.ControllerState;
 import appeng.api.util.AECableType;
 import appeng.me.GridAccessException;
+import appeng.tile.events.AETileEventHandler;
 import appeng.tile.grid.AENetworkPowerTile;
 import appeng.tile.inventory.AppEngInternalInventory;
 import appeng.tile.inventory.InvOperation;
+import lombok.Getter;
+import lombok.Setter;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.ForgeDirection;
 import rocks.spud.mc.basedefense.BaseDefenseModification;
 import rocks.spud.mc.basedefense.api.registry.annotation.common.BlockEntityType;
 import rocks.spud.mc.basedefense.api.registry.annotation.common.RegistrationCriteria;
 import rocks.spud.mc.basedefense.api.registry.criteria.SurveillanceFeatureCriteria;
 import rocks.spud.mc.basedefense.api.surveillance.network.cache.ISecurityGridCache;
+import rocks.spud.mc.basedefense.api.surveillance.network.event.communication.SecurityDoorRequestEvent;
 import rocks.spud.mc.basedefense.api.surveillance.network.event.controller.SecurityControllerUpdateEvent;
+import rocks.spud.mc.basedefense.common.block.surveillance.SecurityDoorBlock;
 import rocks.spud.mc.basedefense.common.network.cache.SecurityGridCache;
 
 import java.util.Arrays;
@@ -64,6 +72,39 @@ public class SecurityDoorControllerBlockEntity extends AENetworkPowerTile {
 	public static final int[] SIDES = new int[0];
 
 	/**
+	 * Stores the active cooldown period.
+	 */
+	@Getter
+	@Setter
+	private int activeCooldown = 0;
+
+	/**
+	 * Stores the entity state.
+	 */
+	@Getter
+	private boolean state = true;
+
+	/**
+	 * Defines the door cooldown.
+	 */
+	@Getter
+	@Setter
+	private int cooldown = 300;
+
+	/**
+	 * Defines the door rotation.
+	 */
+	@Getter
+	@Setter
+	private int doorRotation = 0;
+
+	/**
+	 * Defines the door extension amount.
+	 */
+	@Getter
+	private int extensionCount = 2;
+
+	/**
 	 * Constructs a new SecurityDoorControllerBlockEntity.
 	 */
 	public SecurityDoorControllerBlockEntity () {
@@ -73,6 +114,52 @@ public class SecurityDoorControllerBlockEntity extends AENetworkPowerTile {
 		this.gridProxy.setFlags (GridFlags.REQUIRE_CHANNEL);
 		this.internalPublicPowerStorage = true;
 		this.internalPowerFlow = AccessRestriction.WRITE;
+
+		this.addNewHandler (new AETileEventHandler () {
+			/**
+			 * {@inheritDoc}
+			 */
+			@Override
+			public void Tick () {
+				super.Tick ();
+
+				// skip ticks on the client side
+				if (worldObj.isRemote) return;
+
+				// handle automatic door closing
+				if (activeCooldown < 0) return;
+				activeCooldown = Math.max (0, (activeCooldown - 1));
+				if (activeCooldown == 0) closeDoor ();
+			}
+
+			/**
+			 * {@inheritDoc}
+			 */
+			@Override
+			public void writeToNBT (NBTTagCompound data) {
+				super.writeToNBT (data);
+
+				data.setInteger ("activeCooldown", activeCooldown);
+				data.setBoolean ("state", state);
+				data.setInteger ("cooldown", cooldown);
+				data.setInteger ("doorRotation", doorRotation);
+				data.setInteger ("extensionCount", extensionCount);
+			}
+
+			/**
+			 * {@inheritDoc}
+			 */
+			@Override
+			public void readFromNBT (NBTTagCompound data) {
+				super.readFromNBT (data);
+
+				activeCooldown = data.getInteger ("activeCooldown");
+				state = data.getBoolean ("state");
+				cooldown = data.getInteger ("cooldown");
+				doorRotation = data.getInteger ("doorRotation");
+				extensionCount = data.getInteger ("extensionCount");
+			}
+		});
 	}
 
 	/**
@@ -153,12 +240,60 @@ public class SecurityDoorControllerBlockEntity extends AENetworkPowerTile {
 	}
 
 	/**
+	 * Handles door requests.
+	 * @param event The event.
+	 */
+	@MENetworkEventSubscribe
+	public void onDoorRequest (SecurityDoorRequestEvent event) {
+		this.activeCooldown = (event.isCooldownEnabled () ? this.cooldown : -1);
+
+		if (event.isState ())
+			this.openDoor ();
+		else
+			this.closeDoor ();
+	}
+
+	/**
 	 * Handles power status updates.
 	 * @param event The event.
 	 */
 	@MENetworkEventSubscribe
 	public void onPowerStatusChange (MENetworkPowerStatusChange event) {
 		this.updateMetadata ();
+	}
+
+	/**
+	 * Opens the door.
+	 */
+	public void openDoor () {
+		if (this.state) return;
+
+		// update blocks
+		for (int i = 1; i <= this.extensionCount; i++) {
+			if (!this.setBlockRelative (i, BaseDefenseModification.getInstance ().getProxy ().getRegistry ().getInstance (SecurityDoorBlock.class), this.doorRotation)) break;
+		}
+		this.state = true;
+
+		// update BE
+		this.worldObj.markTileEntityChunkModified (this.xCoord, this.yCoord, this.zCoord, this);
+		this.worldObj.markBlockForUpdate (this.xCoord, this.yCoord, this.zCoord);
+	}
+
+	/**
+	 * Closes the door.
+	 */
+	public void closeDoor () {
+		if (!this.state) return;
+
+		// update blocks
+		for (int i = 1; i <= this.extensionCount; i++) {
+			if (!this.setBlockRelative (i, Blocks.air, 0)) break;
+		}
+		this.state = false;
+
+		// update BE
+		this.worldObj.markTileEntityChunkModified (this.xCoord, this.yCoord, this.zCoord, this);
+		this.worldObj.markBlockForUpdate (this.xCoord, this.yCoord, this.zCoord);
 	}
 
 	/**
@@ -170,6 +305,56 @@ public class SecurityDoorControllerBlockEntity extends AENetworkPowerTile {
 
 		this.updateMetadata ();
 	}
+
+	/**
+	 * Sets a block relative to the BE position.
+	 * @param distance The distance.
+	 * @param blockType The block type.
+	 * @param metadata The block metadata.
+	 */
+	protected boolean setBlockRelative (int distance, Block blockType, int metadata) {
+		int x = this.xCoord;
+		int y = this.yCoord;
+		int z = this.zCoord;
+
+		switch (this.getRotation ()) {
+			case DOWN:
+				y -= distance;
+				break;
+			case UP:
+				y += distance;
+				break;
+			case NORTH:
+				z -= distance;
+				break;
+			case SOUTH:
+				z += distance;
+				break;
+			case WEST:
+				x -= distance;
+				break;
+			case EAST:
+				x += distance;
+				break;
+		}
+
+		if (this.worldObj.isAirBlock (x, y, z) && !(this.worldObj.getBlock (x, y, z) instanceof SecurityDoorBlock)) return false;
+		this.worldObj.setBlock (x, y, z, blockType, metadata, 2);
+		return true;
+	}
+
+	/**
+	 * Sets the extension count.
+	 * @param extensionCount The extension count.
+	 */
+	public void setExtensionCount (int extensionCount) {
+		state = this.isState ();
+		if (state) this.closeDoor ();
+		this.extensionCount = extensionCount;
+		if (state) this.openDoor ();
+	}
+
+
 
 	/**
 	 * Updates the block metadata.
