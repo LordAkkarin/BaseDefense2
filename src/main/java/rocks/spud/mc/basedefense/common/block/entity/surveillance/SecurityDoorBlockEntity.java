d7d92e0844eb14d4088650844a99ba5c5014a550
Johannes Donath <johannesd@torchmind.com>
2015-01-25 15:22:27 +0100

    Added block decay to doors

--- src/main/java/rocks/spud/mc/basedefense/common/block/entity/surveillance/SecurityDoorBlockEntity.java
+++ src/main/java/rocks/spud/mc/basedefense/common/block/entity/surveillance/SecurityDoorBlockEntity.java
@@ -16,10 +16,16 @@
 
 package rocks.spud.mc.basedefense.common.block.entity.surveillance;
 
+import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.util.ForgeDirection;
 import rocks.spud.mc.basedefense.api.registry.annotation.common.BlockEntityType;
 import rocks.spud.mc.basedefense.api.registry.annotation.common.RegistrationCriteria;
 import rocks.spud.mc.basedefense.api.registry.criteria.SurveillanceFeatureCriteria;
+import rocks.spud.mc.basedefense.common.block.surveillance.SecurityDoorBlock;
+import rocks.spud.mc.basedefense.common.block.surveillance.SecurityDoorControllerBlock;
+
+import java.util.Random;
 
 /**
  * Provides a dummy block entity for security doors.
@@ -33,4 +39,85 @@ public class SecurityDoorBlockEntity extends TileEntity {
 	 * Defines the block entity identifier.
 	 */
 	public static final String IDENTIFIER = "surveillance_security_door";
+
+	/**
+	 * Stores the decay timer.
+	 */
+	private int decayTimer = 20;
+
+	/**
+	 * Constructs a new SecurityDoorBlockEntity.
+	 */
+	public SecurityDoorBlockEntity () {
+		super ();
+
+		this.decayTimer = (new Random ()).nextInt (120);
+		this.decayTimer = Math.max (20, this.decayTimer);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void updateEntity () {
+		super.updateEntity ();
+
+		// skip client side execution
+		if (this.worldObj.isRemote) return;
+
+		// update timer
+		this.decayTimer = Math.max (0, (this.decayTimer - 1));
+		if (decayTimer > 0) return;
+
+		int metadata = this.worldObj.getBlockMetadata (this.xCoord, this.yCoord, this.zCoord);
+		Block block = null;
+		int blockMetadata = 0;
+
+		// handle rotation around Y axis
+		if (metadata <= 4) {
+			if ((metadata & 0x1) != 0x1) {
+				block = this.worldObj.getBlock (this.xCoord, (this.yCoord - 1), this.zCoord);
+				metadata = this.worldObj.getBlockMetadata (this.xCoord, (this.yCoord - 1), this.zCoord);
+			} else {
+				block = this.worldObj.getBlock (this.xCoord, (this.yCoord + 1), this.zCoord);
+				metadata = this.worldObj.getBlockMetadata (this.xCoord, (this.yCoord + 1), this.zCoord);
+			}
+		} else {
+			ForgeDirection direction = ForgeDirection.getOrientation ((metadata - 2));
+
+			int x = this.xCoord;
+			int z = this.zCoord;
+
+			switch (direction) {
+				case NORTH:
+					z--;
+					break;
+				case SOUTH:
+					z++;
+					break;
+				case WEST:
+					x--;
+					break;
+				case EAST:
+					x++;
+					break;
+			}
+
+			block = this.worldObj.getBlock (x, this.yCoord, z);
+			blockMetadata = this.worldObj.getBlockMetadata (x, this.yCoord, z);
+		}
+
+		// decay
+		if (block == null || (!(block instanceof SecurityDoorBlock) && !(block instanceof SecurityDoorControllerBlock))) {
+			this.worldObj.setBlockToAir (this.xCoord, this.yCoord, this.zCoord);
+			return;
+		}
+
+		if (block instanceof SecurityDoorControllerBlock && (metadata & 0x1) != 0x1) {
+			this.worldObj.setBlockToAir (this.xCoord, this.yCoord, this.zCoord);
+			return;
+		}
+
+		this.decayTimer = 20;
+	}
 }
