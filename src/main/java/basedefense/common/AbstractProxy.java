a72ad7916c23ef2ffcb59b0609a430e2ee39f614
Johannes Donath <johannesd@torchmind.com>
2015-05-10 14:19:12 +0200

    Added the corresponding proxy implementations

---
+++ src/main/java/basedefense/common/AbstractProxy.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2015 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package basedefense.common;
+
+import basedefense.common.component.ComponentManager;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import lombok.Getter;
+
+/**
+ * Provides a base proxy implementation.
+ * @author Johannes Donath <a href="mailto:johannesd@torchmind.com">johannesd@torchmind.com</a>
+ */
+public abstract class AbstractProxy {
+        @Getter
+        private final ComponentManager componentManager = new ComponentManager ();
+
+        public AbstractProxy () {
+                this.registerComponents ();
+        }
+
+        /**
+         * Handles the modification pre-initialization.
+         * @param event The event.
+         */
+        public void onPreInitialization (FMLPreInitializationEvent event) {
+                this.componentManager.activateComponents (event);
+        }
+
+        /**
+         * Handles the modification initialization.
+         * @param event The event.
+         */
+        public void onInitialization (FMLInitializationEvent event) {
+                this.componentManager.run (c -> {
+                        c.registerBlocks ();
+                        c.registerItems ();
+
+                        c.registerBlockEntities ();
+                        c.registerEntities ();
+                });
+        }
+
+        /**
+         * Handles the modification post-initialization.
+         * @param event The event.
+         */
+        public void onPostInitialization (FMLPostInitializationEvent event) {
+
+        }
+
+        /**
+         * Registers all modification components.
+         */
+        protected abstract void registerComponents ();
+}
---
+++ src/main/java/basedefense/common/CommonProxy.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package basedefense.common;
+
+import basedefense.client.component.ClientSurveillanceComponent;
+import basedefense.server.component.ServerSurveillanceComponent;
+
+/**
+ * Provides instructions required on both {@link cpw.mods.fml.relauncher.Side#CLIENT} and {@link cpw.mods.fml.relauncher.Side#SERVER}.
+ * @author Johannes Donath <a href="mailto:johannesd@torchmind.com">johannesd@torchmind.com</a>
+ */
+public class CommonProxy extends AbstractProxy {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        protected void registerComponents () {
+                this.getComponentManager ().registerComponent (ServerSurveillanceComponent.class, ClientSurveillanceComponent.class);
+        }
+}
