759c670b994353160f15ebc9a8096be3914bf24f
Johannes Donath <johannesd@torchmind.com>
2015-05-11 19:47:57 +0200

    Added vial item implementation

---
+++ src/main/java/basedefense/common/item/surveillance/VialItem.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2015 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package basedefense.common.item.surveillance;
+
+import basedefense.common.creative.SurveillanceCreativeTab;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.IIcon;
+
+import java.util.List;
+
+/**
+ * @author Johannes Donath <a href="mailto:johannesd@torchmind.com">johannesd@torchmind.com</a>
+ */
+public class VialItem extends Item {
+        public static final VialItem ITEM = new VialItem ();
+        public static final String NAME = "surveillance_vial";
+        public static final String TRANSLATION = "basedefense.surveillance.vial";
+
+        private IIcon icon_empty;
+        private IIcon icon_player;
+        private IIcon icon_entity;
+
+        private VialItem () {
+                super ();
+
+                this.setUnlocalizedName (TRANSLATION);
+                this.setCreativeTab (SurveillanceCreativeTab.TAB);
+                this.setHasSubtypes (true);
+
+                this.setMaxStackSize (1);
+                this.setMaxDamage (0);
+                this.setNoRepair ();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @SuppressWarnings ("unchecked")
+        @Override
+        public void addInformation (ItemStack p_77624_1_, EntityPlayer p_77624_2_, List p_77624_3_, boolean p_77624_4_) {
+                super.addInformation (p_77624_1_, p_77624_2_, p_77624_3_, p_77624_4_);
+
+                NBTTagCompound compound = p_77624_1_.getTagCompound ();
+                if (compound == null) return;
+
+                switch (p_77624_1_.getItemDamage ()) {
+                        case 1:
+                                if (!compound.hasKey ("playerName")) return;
+                                p_77624_3_.add (String.format (LanguageRegistry.instance ().getStringLocalization ("basedefense.gui.surveillance.vial.player"), compound.getString ("playerName")));
+                                break;
+                        case 2:
+                                p_77624_3_.add (LanguageRegistry.instance ().getStringLocalization ("basedefense.gui.surveillance.vial.entity"));
+                                break;
+                }
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @SuppressWarnings ("unchecked")
+        @Override
+        public void getSubItems (Item p_150895_1_, CreativeTabs p_150895_2_, List p_150895_3_) {
+                super.getSubItems (p_150895_1_, p_150895_2_, p_150895_3_);
+
+                p_150895_3_.add (new ItemStack (p_150895_1_, 1, 1));
+                p_150895_3_.add (new ItemStack (p_150895_1_, 1, 2));
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public IIcon getIconFromDamage (int p_77617_1_) {
+                switch (p_77617_1_) {
+                        case 1:
+                                return this.icon_player;
+                        case 2:
+                                return this.icon_entity;
+                        default:
+                                return this.icon_empty;
+                }
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public IIcon getIconIndex (ItemStack p_77650_1_) {
+                return this.getIconFromDamage (p_77650_1_.getItemDamage ());
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void registerIcons (IIconRegister p_94581_1_) {
+                this.icon_empty = p_94581_1_.registerIcon ("basedefense2:vial_empty");
+                this.icon_player = p_94581_1_.registerIcon ("basedefense2:vial_dna_player");
+                this.icon_entity = p_94581_1_.registerIcon ("basedefense2:vial_dna_mob");
+        }
+}
