6062f0448023345bdaada3c695e22910fc1933a0
Johannes Donath <johannesd@torchmind.com>
2015-05-14 19:44:43 +0200

    Added an event for controller changes

---
+++ src/api/java/basedefense/api/surveillance/event/SurveillanceControllerChange.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package basedefense.api.surveillance.event;
+
+import basedefense.api.surveillance.ISurveillanceGridCache.ControllerState;
+import basedefense.api.surveillance.ISurveillanceNetworkController;
+import lombok.Getter;
+import lombok.NonNull;
+
+/**
+ * Provides an event for updates to the {@link ISurveillanceNetworkController}.
+ * @author Johannes Donath <a href="mailto:johannesd@torchmind.com">johannesd@torchmind.com</a>
+ */
+public class SurveillanceControllerChange extends SurveillanceNetworkEvent {
+        @Getter
+        private final ISurveillanceNetworkController controller;
+        @Getter
+        private final ControllerState state;
+
+        public SurveillanceControllerChange (ISurveillanceNetworkController controller, @NonNull ControllerState state) {
+                super ();
+
+                this.controller = controller;
+                this.state = state;
+        }
+}
